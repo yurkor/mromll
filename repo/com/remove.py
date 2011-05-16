7b11d2c91f68fcedf9f0c6fa381a6118e95fdff5
yurkor.83@gmail.com <yurkor.83@gmail.com@d2fd3a09-4ad4-e7d6-13d1-2122e352b333>
2011-05-16 14:41:57 +0000

    commit data

---
+++ repo/com/remove.py
@@ -0,0 +1,19 @@
+﻿print "hello world"
+print "hello world"
+import os, stat
+import shutil
+
+def remove(dir, all_inside=False):
+	if dir.endswith(".svn"):
+		print dir
+		os.chmod(dir, stat.S_IWRITE)  
+		shutil.rmtree(dir)
+		return		
+	for f in os.listdir(dir):
+		sub_dir = dir + "\\" + f
+		if not os.path.isdir(sub_dir):
+			continue		
+		remove(sub_dir)
+		
+		
+remove(".")		

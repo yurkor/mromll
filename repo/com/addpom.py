d087c457df473b87b104b8905d3b228859d78a1a
yurkor.83@gmail.com <yurkor.83@gmail.com@d2fd3a09-4ad4-e7d6-13d1-2122e352b333>
2011-05-20 12:07:24 +0000

    ffgf

---
+++ repo/com/addpom.py
@@ -0,0 +1,35 @@
+﻿import os, stat
+import re
+import shutil
+
+reg = re.compile("\\\\(\w+?)\\\\((\d|\.)+)$")
+def visit_dirs(dir):
+	match = re.search(reg, dir)
+	if match != None:
+		version = match.group(2)
+		name = 	match.group(1)	
+		print ("%s %s" % (name, version))
+		str = """
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>com.ml</groupId>
+    <artifactId>{name}</artifactId>
+    <version>{version}</version>
+    <packaging>jar</packaging>
+</project>		
+		""".format(name=name, version=version)
+		print str
+		file = open("%s\\%s-%s.pom" % (dir,name, version), "w")
+		file.write(str)
+		file.close()
+		return		
+	for f in os.listdir(dir):
+		sub_dir = dir + "\\" + f
+		if not os.path.isdir(sub_dir):
+			continue		
+		visit_dirs(sub_dir)
+	
+visit_dirs(".")
